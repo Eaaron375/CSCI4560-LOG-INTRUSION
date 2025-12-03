c3fa27facd3b613a27098bacb1785c75c7f6088a
Eaaron375 <eaf4i@mtmail.mtsu.edu>
2025-12-02 23:33:06 -0600

    Update log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -1,27 +1,33 @@
+# log_parser.py
 from faker import Faker
 import random, datetime, mysql.connector, time
 
 fake = Faker()
 
-# Connect to MySQL
 conn = mysql.connector.connect(
-    host="127.0.0.1",
-    port=3306,  # host-mapped port
+    host="localhost",
+    port=3306,
     user="root",
-    password="password",
-    database="ids_db"
+    password="PASSWORD",
+    database="Project"
 )
 cursor = conn.cursor()
 
-while True:
-    user = fake.user_name()
+print(" Connected to MySQL (Project.LOGINS)")
+
+while True: # Continuously generate and insert new simulated login events
+    username = fake.user_name()
     ip = fake.ipv4_public()
     status = random.choice(["success", "failure"])
     event_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    query = "INSERT INTO LOGINS (user_id, event_time, ip_address, status) VALUES (%s, %s, %s, %s)"
+    # for now: always user_id = 1  
+    query = """
+        INSERT INTO LOGINS (user_id, event_time, ip_address, status, processed)
+        VALUES (%s, %s, %s, %s, 0)
+    """
     cursor.execute(query, (1, event_time, ip, status))
     conn.commit()
 
-    print(f"Inserted: {user} | {ip} | {status}")
-    time.sleep(2)  # Simulate real-time incoming logs
+    print(f"Inserted → {event_time} | {username} | {ip} | {status}")
+    time.sleep(1)
