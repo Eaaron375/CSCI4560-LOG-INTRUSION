34d90cef42ba7175040c678218a56d0ac04854c1
Eaaron375 <eaf4i@mtmail.mtsu.edu>
2025-12-02 23:19:36 -0600

    Update generate_security_events.py

--- generate_security_events.py
+++ generate_security_events.py
@@ -11,7 +11,7 @@ fake = Faker()
 # ---------------------------
 DB_CONFIG = {
     "user":     "root",     
-    "password": "@eaf4i12", 
+    "password": "PASSWORD", 
     "host":     "127.0.0.1",
     "database": "Project"
 }
